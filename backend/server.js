908a52047d46e10607eeb8de9e1d7faac9692a11
ntua-el20614 <el20614@mail.ntua.gr>
2023-11-03 14:59:06 +0200

    node server.js works // CHANGE PORT

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("ELEOS")
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+
+// Start the server
+const port = "5500";
+const ipAddress = 'localhost';//
+app.listen(port, ipAddress, () => {
+  console.log(`Server is running on http://${ipAddress}:${port}`);//${ipAddress}:
+});
