46178d3ccdb2ab27cfddf64c4922eb4b5d57f69a
ntua-el20614 <el20614@mail.ntua.gr>
2023-12-22 01:46:58 +0200

    error with middleware

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -1,13 +1,14 @@
-const authorize = (req, res, next) => {
-    const secretKey = req.body.secretKey;
-    const admin = req.body.is_user_admin;
+const authorize = (req, res, next) => { 
   
-    // Check if the API key is valid (this is a simple example)
-    if (admin && secretKey !== "3141592653589793236264") {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-  
-    next();
+  const secretKey = req.body.secretKey;
+  const admin = req.body.is_user_admin;
+
+  // Check if the API key is valid (this is a simple example)
+  if (!admin || secretKey !== "3141592653589793236264") {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  next();
 }
 
 module.exports = authorize;
