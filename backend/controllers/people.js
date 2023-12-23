a021bd5a87262c4f86733c21d4fd28f99dbab283
ntua-el20605 <el20605@mail.ntua.gr>
2023-12-23 20:45:57 +0200

    Did query d and ai log for it

--- backend/controllers/people.js
+++ backend/controllers/people.js
@@ -31,6 +31,8 @@ exports.getTypeOfPeople = (req, res, next) => {
     });
 
 }
+
+
 exports.getTypeOfPeopleOneProfession = (req, res, next) => {
     const profession = req.params.profession;
 
@@ -40,6 +42,50 @@ exports.getTypeOfPeopleOneProfession = (req, res, next) => {
             connection.release();
             if (err) return res.status(500).json({ message: 'Internal server error' });
 
+            return res.status(200).json(rows);
+        });
+    });
+
+}
+
+
+exports.getAllInfoForAPerson = (req, res, next) => {
+    const nameID = req.params.nameID;
+
+
+    const query = `
+        SELECT 
+            p.nconst AS nameID,
+            p.primaryName AS name,
+            p.img_url_asset AS namePoster,
+            p.birthYear AS birthYr,
+            p.deathYear AS deathYr,
+            p.primaryProfession AS profession,
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'titleID', tp.tconst,
+                    'category', tp.category
+                )
+            ) AS nameTitles
+        FROM 
+            people p
+        JOIN 
+            title_principals tp ON p.nconst = tp.nconst
+        WHERE 
+            p.nconst = '${nameID}'
+        GROUP BY 
+            p.nconst;
+    `;
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error connecting to database' });
+        }
+        connection.query(query, (err, rows) => {
+            connection.release();
+            if (err) {
+                return res.status(500).json({ message: 'Internal server error' });
+            }
             return res.status(200).json(rows);
         });
     });
--- backend/routes/people.js
+++ backend/routes/people.js
@@ -8,4 +8,7 @@ router.get('/people', peopleController.getPeople);
 router.get('/people/profession/:profession', peopleController.getTypeOfPeople);
 router.get('/people/one_profession/:profession', peopleController.getTypeOfPeopleOneProfession);
 
+//d
+router.get('/name/:nameID', peopleController.getAllInfoForAPerson)
+
 module.exports = router;
