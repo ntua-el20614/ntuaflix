fdf341beb6b0aab59b01ea45b5c7e4a487a3345e
el20614 <el20614@mail.ntua.gr>
2024-01-06 00:09:35 +0200

    database.js

--- backend/utils/testConnection.js
+++ backend/utils/testConnection.js
@@ -1,22 +1,13 @@
 const mysql = require('mysql2/promise');
 
-async function testConnection() {
-    try {
-        const pool = mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            port: 3306,
-            password: 'password',
-            database: 'ntuaflix',
-            connectionLimit: 100
-        });
-
-        const [results] = await pool.query('SELECT 1');
-        console.log('Connection successful:', results);
-        pool.end();
-    } catch (error) {
-        console.error('Connection failed:', error);
-    }
-}
-
-testConnection();
+/* create connection and export it */
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: 'password',
+    database: 'ntuaflix',
+    connectionLimit: 100 // Adjust the limit as per your requirements
+  });
+  
+module.exports = { pool };
