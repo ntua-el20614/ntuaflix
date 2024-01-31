8c712cbb13409485bc9dadaa1e941df27a0ae74f
el20614 <el20614@mail.ntua.gr>
2024-01-31 19:59:48 +0200

    minor issues

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,19 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['token']; // or 'Authorization' if you follow the standard header
+    
+    const token = req.headers['authorization']?.split(' ')[1]; // Common practice to use 'Bearer <token>'
 
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || '3141592653589793236264');
-        
-        // If token is valid, store the decoded information (e.g., userId) in the request
-        // for use in your route handlers
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || '1234');
         req.userId = decoded.userId;
-
         next();
     } catch (error) {
         return res.status(401).json({ message: 'Unauthorized', error: error.message });
