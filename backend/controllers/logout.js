3fe78d8a198d336d19f6a9f2bb7cc15bd0d83eab
el20614 <el20614@mail.ntua.gr>
2024-01-31 19:57:01 +0200

    logout doesnt need anything

--- backend/controllers/logout.js
+++ backend/controllers/logout.js
@@ -1,14 +1,7 @@
 // controllers/logout.js
 
 exports.logout = async (req, res) => {
-    try {
-        // Perform any server-side cleanup if needed
-        // For example, invalidating server-side session, if you use sessions
 
-        // Respond to the client indicating successful logout
-        res.status(200).json({ message: 'Logout successful' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Something went wrong with the server.' });
-    }
+    res.status(200).json({ message: 'Logout successful' });
+     
 };
