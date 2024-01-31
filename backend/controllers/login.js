8750811c254fca05606b76f4cbea03786499bcd2
el20614 <el20614@mail.ntua.gr>
2024-01-31 19:59:24 +0200

    default is false

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -55,7 +55,7 @@ exports.signup = async (req, res) => {
     const query = `INSERT INTO users (username, password_hashed, approved) 
     VALUES (?, ?, ?)`;
     try {
-        const [results] = await pool.query(query, [username, hashedPassword, 1]);
+        const [results] = await pool.query(query, [username, hashedPassword, 0]);
         res.status(200).json({ message: 'Operation successful', data: results });
     } catch (err) {
         res.status(500).json({ message: 'Internal server error', error: err.message });
